51926f85dddc503265f83b64d4db0d746ce65cb0
ZhongXiLu <zhongxilu@hotmail.com>
2019-11-18 14:58:28 +0100

    Fixed workflow?

--- unit_tests.sh
+++ unit_tests.sh
@@ -6,5 +6,5 @@ ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 for SERVICE in ${ROOT_DIR}/services/*
 do
     echo "Testing $(basename ${SERVICE})"
-    docker-compose -f ${ROOT_DIR}/docker-compose-dev.yml exec $(basename ${SERVICE}) python manage.py test
+    docker-compose -f ${ROOT_DIR}/docker-compose-dev.yml exec -T $(basename ${SERVICE}) python manage.py test
 done
