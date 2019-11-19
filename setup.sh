50cc7a9ef7d4346e6d8ed7a91660eaea6904a467
ZhongXiLu <zhongxilu@hotmail.com>
2019-11-19 15:37:07 +0100

    Fixed Minikube workflow

--- setup.sh
+++ setup.sh
@@ -7,6 +7,6 @@ for SERVICE in ${ROOT_DIR}/services/*
 do
     echo "Recreating database: $(basename ${SERVICE})"
     POD=$(kubectl get pods --all-namespaces | grep $(basename ${SERVICE})'-' | grep -v 'db-' | awk '{print $2}')
-    kubectl exec -it $POD python manage.py recreate_db
-    kubectl exec -it $POD python manage.py seed_db
+    kubectl exec -i $POD python manage.py recreate_db
+    kubectl exec -i $POD python manage.py seed_db
 done
