376545ce86765a5393a31ff9ebaa60f3328de5e4
ZhongXiLu <zhongxilu@hotmail.com>
2019-11-19 15:15:43 +0100

    Fixed Minikube workflow

--- runAllTests.sh
+++ runAllTests.sh
@@ -3,5 +3,5 @@ ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 for SERVICE in ${ROOT_DIR}/services/*
 do
     POD=$(kubectl get pods --all-namespaces | grep $(basename ${SERVICE})'-' | grep -v 'db-' | awk '{print $2}')
-    kubectl exec -it $POD python manage.py test
+    sudo kubectl exec -it $POD python manage.py test
 done
