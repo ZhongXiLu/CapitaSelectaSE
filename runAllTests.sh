ee112b7051f6c9abd73e648adfbe7017359114b6
ZhongXiLu <zhongxilu@hotmail.com>
2019-11-19 15:07:45 +0100

    Updated workflow and some scripts

--- runAllTests.sh
+++ runAllTests.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
-
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-# Recreate and seed the database for each service
 for SERVICE in ${ROOT_DIR}/services/*
 do
-    echo "Recreating database: $(basename ${SERVICE})"
     POD=$(kubectl get pods --all-namespaces | grep $(basename ${SERVICE})'-' | grep -v 'db-' | awk '{print $2}')
-    kubectl exec -it $POD python manage.py recreate_db
-    kubectl exec -it $POD python manage.py seed_db
+    kubectl exec -it $POD python manage.py test
 done
